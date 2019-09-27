b7b5d90c3aab29765c7755aa16a3cf7555e9d196
Mauricio Aristizabal Cano <mauriaristi@gmail.com>
2019-09-27 13:24:19 -0500

    Minor updates.

---
+++ tests/jupyter_notebook/fem_tests/fem_element.py
@@ -0,0 +1,34 @@
+
+
+
+# What do we need to load with an element:
+#   - Basis functions.
+#   - Basic properties:
+#     * DOF (Number of nodes).
+#     * Dimension.
+#     * Dimension.
+
+
+
+# Base class of a Finite Element.
+class felem:
+
+
+  # Attributes:
+
+  #   Number of basis functions.
+  #   Number of basis functions.
+  #   Number of basis functions.
+  #   Number of basis functions.
+  #   Number of basis functions.
+
+  # Member functions:
+
+  def __init__(self):
+
+
+
+  # end function
+
+
+

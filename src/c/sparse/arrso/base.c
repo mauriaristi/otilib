363721231e833a722a819b80571058be157852d0
Mauricio Aristizabal Cano <mauriaristi@gmail.com>
2020-01-07 18:20:05 -0500

    Minor update sarr class

--- src/c/sparse/arrso/base.c
+++ src/c/sparse/arrso/base.c
@@ -10,21 +10,12 @@
 // } sotinum_t;             ///< Sparse OTI number type
 
 // typedef struct {
-//     coeff_t          re; ///< Real coefficient.
-//     coeff_t**      p_im; ///< Array with all imaginary coefficients per order.
-//     imdir_t**     p_idx; ///< Directions associated to each coefficient per order.
-//     ndir_t*       p_nnz; ///< Number of non zero coefficients per order.
-// } somin_t;               ///< Minimal sparse OTI number type. 
-
-// typedef struct {
-//     somin_t*      p_data; ///< Pointer to array of Sparse otinums.
+//     sotinum_t*    p_data; ///< Pointer to array of Sparse otinums.
 //     uint64_t       nrows; ///< Number of rows.
 //     uint64_t       ncols; ///< Number of cols.
 //     uint64_t        size; ///< Size of array.
-//     ndir_t*       p_size; ///< Allocated size per order.
-//     ord_t          order; ///< Truncation order of the number.
 //     flag_t          flag; ///< Memory flag.
-// } arrso_t;                ///< Array of OTIs type.
+// } sarr_t;                ///< Array of OTIs type.
 
 
 
@@ -35,9 +26,9 @@
 
 // Copy operations.
 // ****************************************************************************************************
-void arrso_copy_to(arrso_t* arr, arrso_t* res, dhelpl_t dhl){
+void sarr_copy_to(sarr_t* arr, sarr_t* res, dhelpl_t dhl){
 
-    somin_t* dummy_src, *dummy_dest;
+    sotinum_t* dummy_src, *dummy_dest;
     ord_t ordi;
     uint64_t i;
     // TODO: add allocation check.
@@ -64,11 +55,11 @@ void arrso_copy_to(arrso_t* arr, arrso_t* res, dhelpl_t dhl){
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-arrso_t arrso_copy(arrso_t* arr, dhelpl_t dhl){
+sarr_t sarr_copy(sarr_t* arr, dhelpl_t dhl){
 
-    arrso_t res = arrso_empty_like(arr,dhl);
+    sarr_t res = sarr_empty_like(arr,dhl);
 
-    arrso_copy_to(arr,&res,dhl);
+    sarr_copy_to(arr,&res,dhl);
 
     return res;
 
@@ -85,41 +76,41 @@ arrso_t arrso_copy(arrso_t* arr, dhelpl_t dhl){
 // Setters.
 // Setter by one index.
 // ****************************************************************************************************
-void arrso_set_all_r( coeff_t num, arrso_t* arr, dhelpl_t dhl){
+void sarr_set_all_r( coeff_t num, sarr_t* arr, dhelpl_t dhl){
     
     uint64_t i;
 
     for (i = 0; i<arr->size; i++ ){        
-        arrso_set_item_i_r( num, i, arr, dhl);
+        sarr_set_item_i_r( num, i, arr, dhl);
     }
 
 }
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-inline void arrso_set_item_i_r( coeff_t num, uint64_t i, arrso_t* arr, dhelpl_t dhl){
+inline void sarr_set_item_i_r( coeff_t num, uint64_t i, sarr_t* arr, dhelpl_t dhl){
 
-    somin_t* dummy_somin;
+    sotinum_t* dummy_sonum;
         
-    dummy_somin     = &arr->p_data[ i ];
-    dummy_somin->re = num;
+    dummy_sonum     = &arr->p_data[ i ];
+    dummy_sonum->re = num;
     
     // Remove all imaginary directions.
-    memset( dummy_somin->p_nnz, 0, arr->order * sizeof(ndir_t) );
+    memset( dummy_sonum->p_nnz, 0, arr->order * sizeof(ndir_t) );
 
 }
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-void arrso_set_item_ij_r( coeff_t num, uint64_t i, uint64_t j, arrso_t* arr, dhelpl_t dhl){
+void sarr_set_item_ij_r( coeff_t num, uint64_t i, uint64_t j, sarr_t* arr, dhelpl_t dhl){
 
-    arrso_set_item_i_r( num, j + i*arr->ncols, arr,  dhl);
+    sarr_set_item_i_r( num, j + i*arr->ncols, arr,  dhl);
 
 }
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-void arrso_set_all_o( sotinum_t* num, arrso_t* arr, dhelpl_t dhl){
+void sarr_set_all_o( sotinum_t* num, sarr_t* arr, dhelpl_t dhl){
 
     uint64_t i;
 
@@ -130,41 +121,41 @@ void arrso_set_all_o( sotinum_t* num, arrso_t* arr, dhelpl_t dhl){
     //     exit( OTI_NotImplemented );
     // }
     for (i = 0; i<arr->size; i++ ){
-        arrso_set_item_i_o( num, i, arr, dhl);
+        sarr_set_item_i_o( num, i, arr, dhl);
     }
 
 }
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-inline void arrso_set_item_i_o( sotinum_t* src, uint64_t i, arrso_t* arr, dhelpl_t dhl){
+inline void sarr_set_item_i_o( sotinum_t* src, uint64_t i, sarr_t* arr, dhelpl_t dhl){
     
-    somin_t* dummy_somin;
+    sotinum_t* dummy_sonum;
     ord_t ordi;    
 
-    dummy_somin     = &arr->p_data[ i ];
-    dummy_somin->re = src->re;
+    dummy_sonum     = &arr->p_data[ i ];
+    dummy_sonum->re = src->re;
 
     // Copy imaginary coefficients
     for ( ordi = 0; ordi < src->order; ordi++){
         // Copy memory to dest number. Only copy non zeros.
-        memcpy(dummy_somin->p_im[ordi], src->p_im[ordi], src->p_nnz[ordi]*sizeof(coeff_t) );
-        memcpy(dummy_somin->p_idx[ordi],src->p_idx[ordi],src->p_nnz[ordi]*sizeof(imdir_t) );
+        memcpy(dummy_sonum->p_im[ordi], src->p_im[ordi], src->p_nnz[ordi]*sizeof(coeff_t) );
+        memcpy(dummy_sonum->p_idx[ordi],src->p_idx[ordi],src->p_nnz[ordi]*sizeof(imdir_t) );
 
-        dummy_somin->p_nnz[ordi] = src->p_nnz[ordi]; 
+        dummy_sonum->p_nnz[ordi] = src->p_nnz[ordi]; 
     }
 
     // Set all other elements in the imaginary directions to zero.
     for ( ; ordi<arr->order; ordi++ ){
-        dummy_somin->p_nnz[ordi] = 0;
+        dummy_sonum->p_nnz[ordi] = 0;
     }
 }
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-void arrso_set_item_ij_o( sotinum_t* num, uint64_t i, uint64_t j, arrso_t* arr, dhelpl_t dhl){
+void sarr_set_item_ij_o( sotinum_t* num, uint64_t i, uint64_t j, sarr_t* arr, dhelpl_t dhl){
 
-    arrso_set_item_i_o( num, j + i*arr->ncols, arr, dhl);
+    sarr_set_item_i_o( num, j + i*arr->ncols, arr, dhl);
 
 }
 // ----------------------------------------------------------------------------------------------------
@@ -181,20 +172,20 @@ void arrso_set_item_ij_o( sotinum_t* num, uint64_t i, uint64_t j, arrso_t* arr, 
 
 // Getters.
 // ****************************************************************************************************
-inline sotinum_t arrso_get_item_i(arrso_t* arr, uint64_t i, dhelpl_t dhl){
+inline sotinum_t sarr_get_item_i(sarr_t* arr, uint64_t i, dhelpl_t dhl){
     
     sotinum_t res;
-    somin_t* dummy_somin;
+    sotinum_t* dummy_sonum;
 
     // Check boundness
     if (i < arr->size){
 
-        dummy_somin = &arr->p_data[i];
+        dummy_sonum = &arr->p_data[i];
 
-        res.re     = dummy_somin->re;
-        res.p_im   = dummy_somin->p_im;
-        res.p_idx  = dummy_somin->p_idx;
-        res.p_nnz  = dummy_somin->p_nnz;
+        res.re     = dummy_sonum->re;
+        res.p_im   = dummy_sonum->p_im;
+        res.p_idx  = dummy_sonum->p_idx;
+        res.p_nnz  = dummy_sonum->p_nnz;
         res.p_size = arr->p_size;
         res.order  = arr->order;
         res.flag   = 0; // No ownership of memory.
@@ -211,9 +202,9 @@ inline sotinum_t arrso_get_item_i(arrso_t* arr, uint64_t i, dhelpl_t dhl){
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-sotinum_t arrso_get_item_ij(arrso_t* arr, uint64_t i, uint64_t j, dhelpl_t dhl){
+sotinum_t sarr_get_item_ij(sarr_t* arr, uint64_t i, uint64_t j, dhelpl_t dhl){
 
-    sotinum_t res = arrso_get_item_i(arr, j + i*arr->ncols, dhl );
+    sotinum_t res = sarr_get_item_i(arr, j + i*arr->ncols, dhl );
 
     return res;
 
@@ -232,10 +223,10 @@ sotinum_t arrso_get_item_ij(arrso_t* arr, uint64_t i, uint64_t j, dhelpl_t dhl){
 
 
 // ****************************************************************************************************
-arrso_t arrso_eye_bases(uint64_t nrows, bases_t nbases, ord_t order, dhelpl_t dhl){
+sarr_t sarr_eye_bases(uint64_t nrows, bases_t nbases, ord_t order, dhelpl_t dhl){
 
     ndir_t p_nnz[_MAXORDER_OTI];
-    arrso_t  res;
+    sarr_t  res;
     uint64_t i;
     ord_t ordi;
 
@@ -243,7 +234,7 @@ arrso_t arrso_eye_bases(uint64_t nrows, bases_t nbases, ord_t order, dhelpl_t dh
         p_nnz[ordi] = dhelp_ndirOrder(nbases,ordi+1);
     }
 
-    res = arrso_zeros( nrows, nrows, p_nnz, order, dhl);
+    res = sarr_zeros( nrows, nrows, p_nnz, order, dhl);
     
     for (i=0; i<res.nrows; i++ ){
         res.p_data[ i + i*res.ncols ].re = 1.0;
@@ -255,10 +246,10 @@ arrso_t arrso_eye_bases(uint64_t nrows, bases_t nbases, ord_t order, dhelpl_t dh
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-arrso_t arrso_ones_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t order, dhelpl_t dhl){
+sarr_t sarr_ones_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t order, dhelpl_t dhl){
 
     ndir_t p_nnz[_MAXORDER_OTI];
-    arrso_t  res;
+    sarr_t  res;
     uint64_t i;
     ord_t ordi;
 
@@ -266,7 +257,7 @@ arrso_t arrso_ones_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t o
         p_nnz[ordi] = dhelp_ndirOrder(nbases,ordi+1);
     }
 
-    res = arrso_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
+    res = sarr_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
 
     for (i=0; i<res.size; i++ ){
         res.p_data[i].re = 1.0;
@@ -278,10 +269,10 @@ arrso_t arrso_ones_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t o
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-arrso_t arrso_zeros_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t order, dhelpl_t dhl){
+sarr_t sarr_zeros_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t order, dhelpl_t dhl){
 
     ndir_t p_nnz[_MAXORDER_OTI];
-    arrso_t  res;
+    sarr_t  res;
     uint64_t i;
     ord_t ordi;
 
@@ -289,7 +280,7 @@ arrso_t arrso_zeros_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t 
         p_nnz[ordi] = dhelp_ndirOrder(nbases,ordi+1);
     }
 
-    res = arrso_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
+    res = sarr_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
 
     for (i=0; i<res.size; i++ ){
         res.p_data[i].re = 0.0;
@@ -304,9 +295,9 @@ arrso_t arrso_zeros_bases(uint64_t nrows, uint64_t ncols, bases_t nbases, ord_t 
 
 
 // ****************************************************************************************************
-arrso_t arrso_eye(uint64_t nrows, const ndir_t* p_nnz, ord_t order, dhelpl_t dhl){
+sarr_t sarr_eye(uint64_t nrows, const ndir_t* p_nnz, ord_t order, dhelpl_t dhl){
 
-    arrso_t  res = arrso_zeros( nrows, nrows, p_nnz, order, dhl);
+    sarr_t  res = sarr_zeros( nrows, nrows, p_nnz, order, dhl);
     uint64_t i;
     
     for (i=0; i<res.nrows; i++ ){
@@ -321,9 +312,9 @@ arrso_t arrso_eye(uint64_t nrows, const ndir_t* p_nnz, ord_t order, dhelpl_t dhl
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-arrso_t arrso_ones(uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t order, dhelpl_t dhl){
+sarr_t sarr_ones(uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t order, dhelpl_t dhl){
 
-    arrso_t  res = arrso_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
+    sarr_t  res = sarr_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
     uint64_t i;
     
     for (i=0; i<res.size; i++ ){
@@ -336,9 +327,9 @@ arrso_t arrso_ones(uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t or
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-arrso_t arrso_zeros(uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t order, dhelpl_t dhl){
+sarr_t sarr_zeros(uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t order, dhelpl_t dhl){
 
-    arrso_t  res = arrso_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
+    sarr_t  res = sarr_createEmpty_predef(nrows, ncols, p_nnz, order, dhl);
     uint64_t i;
     
     for (i=0; i<res.size; i++ ){
@@ -351,9 +342,9 @@ arrso_t arrso_zeros(uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t o
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-inline arrso_t arrso_init(void){
+inline sarr_t sarr_init(void){
 
-    arrso_t res;
+    sarr_t res;
 
     res.p_data = NULL;
     res.p_size = NULL;
@@ -369,22 +360,22 @@ inline arrso_t arrso_init(void){
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-void arrso_free(arrso_t* arr){
+void sarr_free(sarr_t* arr){
     
     if (arr->flag && 1){
         free(arr->p_data);
     }
 
-    (*arr) = arrso_init();
+    (*arr) = sarr_init();
 
 }
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-inline arrso_t arrso_empty_like(arrso_t* arr, dhelpl_t dhl){
+inline sarr_t sarr_empty_like(sarr_t* arr, dhelpl_t dhl){
 
-    arrso_t  res;
-    res = arrso_createEmpty_predef(arr->nrows, arr->ncols, arr->p_size, arr->order, dhl);
+    sarr_t  res;
+    res = sarr_createEmpty_predef(arr->nrows, arr->ncols, arr->p_size, arr->order, dhl);
     return res;
 
 }
@@ -392,14 +383,14 @@ inline arrso_t arrso_empty_like(arrso_t* arr, dhelpl_t dhl){
 
 
 // ****************************************************************************************************
-inline arrso_t arrso_createEmpty_predef(uint64_t nrows, uint64_t ncols, 
+inline sarr_t sarr_createEmpty_predef(uint64_t nrows, uint64_t ncols, 
                         const ndir_t* p_nnz, ord_t order, dhelpl_t dhl){
 
-    arrso_t  res = arrso_init();
+    sarr_t  res = sarr_init();
     void*    memory = NULL;
     size_t   allocation_size;
 
-    allocation_size = arrso_memory_size( nrows * ncols, p_nnz, order);
+    allocation_size = sarr_memory_size( nrows * ncols, p_nnz, order);
 
     // Allocate memory and check if correctly allocated.
     memory = malloc(allocation_size);
@@ -408,7 +399,7 @@ inline arrso_t arrso_createEmpty_predef(uint64_t nrows, uint64_t ncols,
         exit(OTI_OutOfMemory); // TODO: Raise error instead of quitting the program.
     }
 
-    arrso_distribute_memory(memory, nrows, ncols, p_nnz, order, 1, &res);
+    sarr_distribute_memory(memory, nrows, ncols, p_nnz, order, 1, &res);
 
     // This is returned uninitialized.
     return res;
@@ -417,13 +408,13 @@ inline arrso_t arrso_createEmpty_predef(uint64_t nrows, uint64_t ncols,
 // ----------------------------------------------------------------------------------------------------
 
 // ****************************************************************************************************
-size_t arrso_memory_size( uint64_t size, const ndir_t* p_nnz, ord_t order){
+size_t sarr_memory_size( uint64_t size, const ndir_t* p_nnz, ord_t order){
 
     size_t allocation_size = 0 ;
     ord_t  i = 0;
 
 
-    allocation_size += size  * sizeof(somin_t); //  Memory for p_data.
+    allocation_size += size  * sizeof(sotinum_t); //  Memory for p_data.
     allocation_size += order * sizeof(ndir_t);  //  Memory for p_size.
     
     if (order != 0){
@@ -451,13 +442,13 @@ size_t arrso_memory_size( uint64_t size, const ndir_t* p_nnz, ord_t order){
 
 
 // ****************************************************************************************************
-void arrso_distribute_memory(void* mem, uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t order, 
-    flag_t flag, arrso_t* res){
+void sarr_distribute_memory(void* mem, uint64_t nrows, uint64_t ncols, const ndir_t* p_nnz, ord_t order, 
+    flag_t flag, sarr_t* res){
     // size_t allocation_size=0;
     uint64_t i;
     ord_t ordi;
     void* memory = mem;
-    somin_t* dummy_somin;
+    sotinum_t* dummy_sonum;
 
     // Set static elements accordingly.
     res->order = order;
@@ -467,8 +458,8 @@ void arrso_distribute_memory(void* mem, uint64_t nrows, uint64_t ncols, const nd
     res->size  = nrows * ncols;
 
     // Distribute memory among the structur pointers.
-    res->p_data  = (somin_t* )memory;
-    memory    += res->size * sizeof(somin_t); // Move memory pointer.
+    res->p_data  = (sotinum_t* )memory;
+    memory    += res->size * sizeof(sotinum_t); // Move memory pointer.
     
     res->p_size  = (ndir_t*  )memory;
     memory    += res->order * sizeof(ndir_t);
@@ -481,27 +472,27 @@ void arrso_distribute_memory(void* mem, uint64_t nrows, uint64_t ncols, const nd
     if (res->order != 0){
         // Loop to distribute memory through all elements of the array.
         for (i = 0; i < res->size; i++){
-            dummy_somin = &res->p_data[i];
-            dummy_somin->p_im  = (coeff_t**)memory;
+            dummy_sonum = &res->p_data[i];
+            dummy_sonum->p_im  = (coeff_t**)memory;
             memory    += res->order * sizeof(coeff_t*);
             
-            dummy_somin->p_idx = (imdir_t**)memory;
+            dummy_sonum->p_idx = (imdir_t**)memory;
             memory    += res->order * sizeof(imdir_t*);
             
-            dummy_somin->p_nnz = (ndir_t*  )memory;
+            dummy_sonum->p_nnz = (ndir_t*  )memory;
             memory    += res->order * sizeof(ndir_t);
             
             for ( ordi = 0; ordi < res->order; ordi++){
             
                 // Distribute memory.
-                dummy_somin->p_im[ordi] = (coeff_t*)memory;
+                dummy_sonum->p_im[ordi] = (coeff_t*)memory;
                 memory += p_nnz[ordi]*sizeof(coeff_t);                 
 
-                dummy_somin->p_idx[ordi]= (imdir_t*)memory;
+                dummy_sonum->p_idx[ordi]= (imdir_t*)memory;
                 memory += p_nnz[ordi]*sizeof(imdir_t);                 
 
                 // Set number of non-zero to 0.
-                dummy_somin->p_nnz[ordi]  = 0; 
+                dummy_sonum->p_nnz[ordi]  = 0; 
             }
 
         }

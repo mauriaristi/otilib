c65e07bac21b895bdd8a84d2405cef0199600f44
Mauricio Aristizabal Cano <mauriaristi@gmail.com>
2020-03-11 18:36:04 -0500

    Updated performance tests fortran.

--- tests/fortran/iterative_matmul/testoti.f90
+++ tests/fortran/iterative_matmul/testoti.f90
@@ -1,64 +1,166 @@
 PROGRAM MAIN
 
-    USE otim5n10
+    USE otim2n3
     IMPLICIT NONE
     INTEGER, PARAMETER :: sizex = 3
-    ! TYPE(ONUMM5N10)       :: X, Y, Z   !>OTI VARIABLE
-    TYPE(ONUMM5N10)       :: A(sizex,sizex), B(sizex,sizex), C(sizex,sizex)   !>OTI VARIABLE
+    TYPE(ONUMM2N3)       :: X, Y, Z   !>OTI VARIABLE
+    TYPE(ONUMM2N3)       :: A(sizex,sizex), B(sizex,sizex), C(sizex,sizex)   !>OTI VARIABLE
     INTEGER :: I
-    REAL(KIND=8)::            &                !>Step
+    INTEGER, PARAMETER :: NITER=1000000
+    REAL(KIND=8)::   NITER_R=NITER,  CPU_TIMES ,      &                !>Step
         time_start(3), time_finish(3)          !>Stores time
+    COMPLEX*16 :: XZ, YZ, ZZ
+    REAL*8 :: XR, YR, ZR
+
+    REAL*8 :: AR(sizex,sizex), BR(sizex,sizex), CR(sizex,sizex)
+    COMPLEX*16 :: AZ(sizex,sizex), BZ(sizex,sizex), CZ(sizex,sizex)
     ! ============================================================
     
     PRINT *, " "
     PRINT *, " THIS MODULE TESTS THE FORTRAN OTI IMPLEMENTATION."
     PRINT *, " "
 
+    XR = 1.5D0
+    XZ = XR
+    X  = XR
 
-    ! X     = 1.5D0
-    ! X%E1  = 1.0D0
+    YR = 5.2D0
+    YZ = YR
+    Y  = YR
 
-    ! PRINT*,X
-
-    ! Y     = 5.2D0
-    ! ! Y%E2  = 1.0D0    
-
-    ! PRINT*, (X*X*X*X*X)
-
-    ! ! CALL PPRINT(Y**5)
-
-    ! ! CALL PPRINT((2.0d0*X+3.0d0*Y)**5)
-
-    ! ! CALL PPRINT( (2.0d0*X+3.0d0*Y)**0.5d0 )
-
-    ! ! ! CALL PPRINT(X)
-    ! ! ! CALL PPRINT(Y)
-    ! ! Z = X*Y
-    ! ! PRINT*, Z
-    ! ! ! CALL PPRINT( SIN(X*Y) )
+    ZR = 5.2D0
+    ZZ = ZR
+    Z  = ZR
 
 
 
-    ! A = 32.0D0 
-    ! B = 0.5D0
-    
-    ! PRINT*,MATMUL(A,B)
-
-    ! Measure time.
+    ! Real Case:
     CALL CPU_TIME(time_start(1))
-    DO i=1,100000
+    DO i=1,NITER
         ! Multiplication with assignation
-        C = MATMUL(A,B)
+        YR = ZR + XR
+        ZR = XR + YR
     END DO
     CALL CPU_TIME(time_finish(1))
 
-    PRINT "(2(A,F20.3,8X))", '(CPU_TIME using array of ONUMM2N3) [ms]:', &!
-                            (time_finish(1)-time_start(1))*1000
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+    PRINT "(2(A,F20.3,8X))", '-- REAL Scalar Addition CPU_TIME [ns]:', &!
+                            CPU_TIMES
+
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        YR = ZR*XR
+        ZR = XR*YR
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+    PRINT "(2(A,F20.3,8X))", '-- REAL Scalar Multiplication CPU_TIME [ns]:', &!
+                            CPU_TIMES
+    
+    ! Measure time.
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        AR = MATMUL(CR,BR)
+        CR = MATMUL(AR,BR)
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+
+    PRINT "(2(A,F20.3,8X))", '-- REAL MATMUL CPU_TIME [ns]:', &!
+                            CPU_TIMES
 
 
-    ! PRINT *, " "
-    ! PRINT *, " PROGRAM ENDED SUCCESSFULLY."
-    ! PRINT *, " "
+
+    PRINT *, " "
+    ! Complex Case:
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        YZ = ZZ+XZ
+        ZZ = XZ+YZ
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+    PRINT "(2(A,F20.3,8X))", '-- COMPLEX Scalar Addition CPU_TIME [ns]:', &!
+                            CPU_TIMES
+
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        YZ = ZZ*XZ
+        ZZ = XZ*YZ
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+    PRINT "(2(A,F20.3,8X))", '-- COMPLEX Scalar Multiplication CPU_TIME [ns]:', &!
+                            CPU_TIMES
+    
+    ! Measure time.
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        AZ = MATMUL(CZ, BZ)
+        CZ = MATMUL(AZ, BZ)
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+
+    PRINT "(2(A,F20.3,8X))", '-- COMPLEX MATMUL CPU_TIME [ns]:', &!
+                            CPU_TIMES
+
+
+    PRINT *, " "    
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        Y = Z + X
+        Z = X + Y
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+    PRINT "(2(A,F20.3,8X))", '-- OTI Scalar Addition CPU_TIME [ns]:', &!
+                            CPU_TIMES
+
+
+
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        Y = Z * X
+        Z = X * Y
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+    PRINT "(2(A,F20.3,8X))", '-- OTI Scalar Multiplication CPU_TIME [ns]:', &!
+                            CPU_TIMES
+    
+
+    ! Measure time.
+    CALL CPU_TIME(time_start(1))
+    DO i=1,NITER
+        ! Multiplication with assignation
+        A = MATMUL(C , B )
+        C = MATMUL(A , B )
+    END DO
+    CALL CPU_TIME(time_finish(1))
+
+    CPU_TIMES = 1.0d9*( time_finish(1) - time_start(1) ) / NITER_R ! In us.
+
+    PRINT "(2(A,F20.3,8X))", '-- OTI MATMUL CPU_TIME [ns]:', &!
+                            CPU_TIMES
+
+    PRINT *, " "
+    PRINT *, " PROGRAM ENDED SUCCESSFULLY."
+    PRINT *, " "
 
 
 

11b9b0cc532c09c3715633a67f25dadb6229f7d6
Mauricio Aristizabal <mauriaristi@gmail.com>
2021-06-02 04:04:28 -0500

    Minor updates in documentation.

--- include/oti/sparse/scalar/structures.h
+++ include/oti/sparse/scalar/structures.h
@@ -5,18 +5,13 @@
 // --------------------------------------      STRUCTURES        --------------------------------------
 // ----------------------------------------------------------------------------------------------------
 
-// typedef struct {
-//     coeff_t          re; ///< Real coefficient.
-//     coeff_t**      p_im; ///< Array with all imaginary coefficients per order.
-// } soticoef_t;            ///< Sparse OTI number type
-
 typedef struct {
     coeff_t          re; ///< Real coefficient.
     coeff_t**      p_im; ///< Array with all imaginary coefficients per order.
     imdir_t**     p_idx; ///< Directions associated to each coefficient per order.
     ndir_t*       p_nnz; ///< Number of non zero coefficients per order.
     ndir_t*      p_size; ///< Allocated size per order.
-    ord_t         order; ///< Order of the number () .
+    ord_t         order; ///< Actual order of the number.
     ord_t        torder; ///< Truncation order of the number.
     flag_t         flag; ///< Memory flag.
 } sotinum_t;             ///< Sparse OTI number type.
